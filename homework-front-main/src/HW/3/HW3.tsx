44d18eaa02779326786f588f7d8acd4953bd995e
Vladimir <k.vovan.n@gmail.com>
2025-03-19 09:58:23 +0200

    name changed hw03-text

--- homework-front-main/src/HW/3/HW3.tsx
+++ homework-front-main/src/HW/3/HW3.tsx
@@ -33,7 +33,7 @@ export const HW3 = () => {
   return (
     <div id={'hw03'}>
       {currentText ? (
-        <h1 id={'hw03-text'}>ЗДЕСЬ ХОТЕЛОСЬ БЫ УВИДЕТЬ ВВОДИМЫЙ ТЕКСТ</h1>
+        <h1 id={'hw03-text'}>{currentText}</h1>
       ) : (
         <h1 id={'hw03-default-text'}>Здесь появится новое дело</h1> // ничего не меняем, здесь все норм
       )}
@@ -41,7 +41,6 @@ export const HW3 = () => {
       <input id={'hw03-input'} type="text" value={currentText} onChange={handleChange} />
 
       {/*<button id={'hw03-button'} onClick={() => {}}> // НЕ ХВАТАТЕТ ФУНКЦИИ*/}
-      {/* тут еще подправить функцию сохранения */}
       <button id={'hw03-button'} onClick={handleSave}>
         Сохранить
       </button>
