be494a96d0212f871bb7b987013e4422cd40bdf6
Vladimir <k.vovan.n@gmail.com>
2025-03-21 14:08:42 +0200

    H4

--- homework-front-main/src/HW/4/Button.tsx
+++ homework-front-main/src/HW/4/Button.tsx
@@ -1,11 +1,15 @@
 type ButtonPropsType = {
-  callBack: any // НУЖНО ПРОТИПИЗИРОВАТЬ
+  // callBack: any // НУЖНО ПРОТИПИЗИРОВАТЬ
+  callBack: Function // НУЖНО ПРОТИПИЗИРОВАТЬ
+  // name: any // НУЖНО ПРОТИПИЗИРОВАТЬ
   name: any // НУЖНО ПРОТИПИЗИРОВАТЬ
 };
 
 export const Button = (props: ButtonPropsType) => {
   const callBackHandler = () => {
     // НУЖНО ДОПИСАТЬ
+    props.callBack()
+    // console.log(props.callBack())
   };
 
   return (
--- homework-front-main/src/HW/4/Input.tsx
+++ homework-front-main/src/HW/4/Input.tsx
@@ -1,14 +1,22 @@
+import { ChangeEvent } from 'react';
+
 type InputPropsType = {
-	currentText: any // НУЖНО ПРОТИПИЗИРОВАТЬ
-	setCurrentText: any // НУЖНО ПРОТИПИЗИРОВАТЬ
+  // currentText: any // НУЖНО ПРОТИПИЗИРОВАТЬ
+  currentText: string
+  // setCurrentText: any // НУЖНО ПРОТИПИЗИРОВАТЬ
+  setCurrentText: Function
 };
 
 export const Input = (props: InputPropsType) => {
-	const onChangeHandler = (event: 'НУЖНО ПРОТИПИЗИРОВАТЬ') => {
-		// НУЖНО ДОПИСАТЬ
-	};
+  // const onChangeHandler = (event: 'НУЖНО ПРОТИПИЗИРОВАТЬ') => {
+  //   // НУЖНО ДОПИСАТЬ
+  // };
+  const onChangeHandler = (event: ChangeEvent<HTMLInputElement>) => {
+    props.setCurrentText(event.currentTarget.value)
+    // console.log(props.currentText)
+  };
 
-	// return (
-	//   <input id={'hw04-input'} type="text" value={props.currentText} onChange={onChangeHandler} />
-	// );
+  return (
+    <input id={'hw04-input'} type="text" value={props.currentText} onChange={onChangeHandler} />
+  );
 };
